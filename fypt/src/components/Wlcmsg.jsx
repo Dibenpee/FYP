9f301cef7d1b03f7b753b489a061dc7ce0287648
Dibenpee <dibyanshuojha@gmail.com>
2025-07-28 19:53:10 +0545

    removed useless things

--- fypt/src/components/Wlcmsg.jsx
+++
@@ -1,9 +0,0 @@
-const Wlcmsg = ({ user }) => {
-  return (
-    <div className="text-center mt-12">
-      <h2 className="text-2xl font-bold">Welcome Back, {user}.</h2>
-    </div>
-  );
-};
-
-export default Wlcmsg;
